a6b056db69deae600cfb35b7122a1b0e713dd5fa
innotuyi <innotuyi67@gmail.com>
2022-03-15 12:52:51 +0200

    adding properties

--- app/Http/Controllers/PropertiesController.php
+++ app/Http/Controllers/PropertiesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Property;
 
 class PropertiesController extends Controller
 {
@@ -68,8 +69,26 @@ class PropertiesController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'type' =>'required',
+            'location' =>'required',
+            'image_path' =>'required',
+            'status' =>'required'
+             
+        ]);
 
-        return "bad";
+        $property = Property::find($id);
+        $type = Property::find($type);
+        $location = $request->input('location');
+        $image_path = $request->input('image_path');
+        $status = $request->input('status');
+
+        $property->type = $type;
+        $property->location = $location;
+        $property->image_path = $image_path;
+        $property->status = $sttus;
+        
+
     
         
     }
