cbc5734f61ae1af8336bbd59094cec9a28d4396f
innotuyi <innotuyi67@gmail.com>
2022-03-15 12:21:41 +0200

    adding update and destroy routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\PropertiesController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::put('/property/{id}', [PropertiesController::class, 'update']);
+Route::delete('/property/{id}',[PropertiesController::class, 'destroy']);
